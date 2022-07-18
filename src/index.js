9c9146164edef940ed4e2d0bb0915c53bf8b9bfc
HANNAKOT62 <2kotyagova6@gmail.com>
2022-07-18 20:11:09 +0300

    added icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import loveW from "./assets/lovewtrmln.jpg";
+import icon from "./assets/logo.png";
 import { log } from "./js/hello";
 import "./js/change_theme.js";
 import "./sass/main.scss";
@@ -6,8 +6,8 @@ import "./js/get_input.js";
 import "./js/clear_buttons.js";
 import "./js/equals.js";
 
-const mainImage = document.getElementById("mainImage");
-mainImage.src = loveW;
+const calculatorIcon = document.getElementById("calculatorIcon");
+calculatorIcon.href = icon;
 
 const hello = require("./js/hello");
 
