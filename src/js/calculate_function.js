3eddcd98b1ec8b33bc85bb92bbe3878e77ea70b3
HANNAKOT62 <2kotyagova6@gmail.com>
2022-07-26 17:36:49 +0300

    cleaned some mess

--- src/js/calculate_function.js
+++ src/js/calculate_function.js
@@ -547,30 +547,4 @@ function calculate(task) {
   return task;
 }
 
-console.log("pizda");
-let task = "10+(3+2+(-3!-4.5)+15-5-(9+(2+1+1)+1))"; // 0.5  //20 не цифр и, //37 символов
-let a = calculate(task); //працуе 0.5
-console.log(a);
-console.log(calculate("21*7*(3-2!)-((2%10-5)/2)+13")); //162,4
-console.log(simpleOperation("12*-4")); //ok
-console.log(calculate("120/(-6)-12+(8+9)")); // ok -15
-console.log(calculate("5^(-3)")); //ok 0.008
-console.log(calculate("12-2!+((2^(-5)+2)-1)+13")); //працуе 24.03125
-console.log(calculate("123-34.5*76-5!*(2^8.8)")); // -55985.62....
-console.log(calculate("9-(-2)^5+32%6")); //42,92
-console.log(calculate("66-9^(18/(-9))")); //65,9876.....
-console.log(calculate("18-3!!+49/7")); //-695
-console.log(calculate("12-5*((8-3^1.1)+45)-9!")); //-363116.258...
-console.log(calculate("2^((((5))))"));
-console.log(calculate("21211221212121+667565656556"));
-try {
-  console.log(calculate("0.047619047619047616+3*10^(4*10^(3))"));
-} catch (e) {
-  console.log(e);
-}
-console.log(calculate("(-6-3)"));
-console.log(calculate("-(-4.143559041588484)^2"));
-console.log(calculate("-(-11)"));
-console.log(calculate("55-333.63+8^3*((-46))*(-8-(-3))"));
-
 module.exports = calculate;
